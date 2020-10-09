9335381d1c744de7a0f418f19dfec02976bdb714
nadakautsar <54785249+nadakautsar@users.noreply.github.com>
2020-10-09 20:40:03 +0700

    Add files via upload

---
+++ sesi7.js
@@ -0,0 +1,24 @@
+var name = "";
+var money = 4000;
+var age = 16;
+
+
+if (name === "" ){
+  console.log("Anda Tidak Boleh Masuk!");
+  debugger;
+  }else{
+  //KURANG DARI 17
+    if (age < 17){
+      console.log("Anda hanya bisa pesan juice");
+      if (money >= 50000 ){
+        console.log("Anda bisa pesan minum. Sisa uang anda:", money - 50000);
+      }else{console.log("Uang Tidak Cukup Anda Harus Pulang");}
+    }else{
+        console.log("Anda hanya bisa pesan anggur");
+      if (money >= 300000 ){
+        console.log("Anda bisa pesan minum. Sisa uang anda:", money - 300000);
+      }else{
+        console.log("Uang tidak cukup anda harus pulang");
+      }
+    }
+  }
